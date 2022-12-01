0311147273810f6653ab476a1b0086015f81a836
Ted Bowman <ted+git@tedbow.com>
2022-11-30 19:56:29 -0500

    prettier and comments

--- rowFilterer.js
+++ rowFilterer.js
@@ -1,35 +1,38 @@
+/**
+ * A filter table rows.
+ */
 class rowFilterer {
-    addHideCondition(element) {
-        const parentRow = element.closest("tr");
-        if (!parentRow.hasOwnProperty("hideConditions")) {
-            parentRow.hideConditions = new Set();
-        }
-        parentRow.hideConditions.add(this.getElementColumnIndex(element));
-        this.setHideStatus(parentRow);
+  addHideCondition(element) {
+    const parentRow = element.closest("tr");
+    if (!parentRow.hasOwnProperty("hideConditions")) {
+      parentRow.hideConditions = new Set();
     }
-    removeHideCondition(element) {
-        const parentRow = element.closest("tr");
-        if (!parentRow.hasOwnProperty("hideConditions")) {
-            return;
-        }
-        parentRow.hideConditions.delete(this.getElementColumnIndex(element));
-        this.setHideStatus(parentRow);
+    parentRow.hideConditions.add(this.getElementColumnIndex(element));
+    this.setHideStatus(parentRow);
+  }
+  removeHideCondition(element) {
+    const parentRow = element.closest("tr");
+    if (!parentRow.hasOwnProperty("hideConditions")) {
+      return;
     }
-    setHideStatus(element) {
-        if (
-            element.hasOwnProperty("hideConditions") &&
-            element.hideConditions.size > 0
-        ) {
-            element.style.display = "none";
-        } else {
-            element.style.display = "table-row";
-        }
+    parentRow.hideConditions.delete(this.getElementColumnIndex(element));
+    this.setHideStatus(parentRow);
+  }
+  setHideStatus(element) {
+    if (
+      element.hasOwnProperty("hideConditions") &&
+      element.hideConditions.size > 0
+    ) {
+      element.style.display = "none";
+    } else {
+      element.style.display = "table-row";
     }
+  }
 
-    getElementColumnIndex(element) {
-        const td = element.closest("td");
-        const tds = Array.from(td.closest("tr").children);
-        return tds.indexOf(td);
-    }
+  getElementColumnIndex(element) {
+    const td = element.closest("td");
+    const tds = Array.from(td.closest("tr").children);
+    return tds.indexOf(td);
+  }
 }
 export { rowFilterer };
--- toolbarRowFilterer.js
+++ toolbarRowFilterer.js
@@ -1,68 +1,83 @@
 let src = chrome.runtime.getURL("rowFilterer.js");
 const { rowFilterer } = await import(src);
 
+/**
+ * A row filter that also has a toolbar element.
+ */
 class toolbarRowFilterer extends rowFilterer {
-
+  /**
+   *
+   * @param displayedElements
+   * @param filterType
+   * @param filterValues
+   * @returns {HTMLDivElement}
+   */
   setUpFilter(displayedElements, filterType, filterValues) {
     if (filterValues === undefined) {
       filterValues = [
         ...new Set(
-            [...displayedElements].map((displayedElement) =>
-                displayedElement.innerText.trim()
-            )
+          [...displayedElements].map((displayedElement) =>
+            displayedElement.innerText.trim()
+          )
         ),
       ];
     }
     const containerDiv = document.createElement("div");
     containerDiv.className = `${filterType}Count`;
-    filterValues.forEach(function (filterValue){
-      let issueInfo = { name: filterValue, count: 0 };
-      displayedElements.forEach((field) => {
-        if (field.textContent.includes(filterValue)) {
-          issueInfo.count++;
-        }
-      });
-      const filterValueDiv = document.createElement("div");
-      filterValueDiv.innerText = `${filterValue}: ${issueInfo.count}`;
-      if (issueInfo.count) {
-        const clicker = document.createElement("a");
-        //clicker.setAttribute('href', '#')
-        clicker.setAttribute("filterValue", filterValue);
-        clicker.onclick = function (event) {
-          const target = event.target;
-          const targetIsFiltered = target.hasAttribute("filtered");
-          document.querySelectorAll(`.${filterType}Count .filter`).forEach((link) => {
-            link.innerText = "🔎";
-            link.removeAttribute("filtered");
-          });
-          if (targetIsFiltered) {
-            displayedElements.forEach(function (displayedElement) {
-              this.removeHideCondition(displayedElement);
-                }.bind(this)
-            );
-            return;
+    filterValues.forEach(
+      function (filterValue) {
+        let issueInfo = { name: filterValue, count: 0 };
+        displayedElements.forEach((field) => {
+          if (field.textContent.includes(filterValue)) {
+            issueInfo.count++;
           }
-
-          target.innerText = "✅";
-          target.setAttribute("filtered", true);
-          displayedElements.forEach(function (displayedElement) {
-            if (displayedElement.innerText.includes(filterValue)) {
-              this.removeHideCondition(displayedElement);
-            } else {
-              this.addHideCondition(displayedElement);
+        });
+        const filterValueDiv = document.createElement("div");
+        filterValueDiv.innerText = `${filterValue}: ${issueInfo.count}`;
+        if (issueInfo.count) {
+          const clicker = document.createElement("a");
+          //clicker.setAttribute('href', '#')
+          clicker.setAttribute("filterValue", filterValue);
+          clicker.onclick = function (event) {
+            const target = event.target;
+            const targetIsFiltered = target.hasAttribute("filtered");
+            document
+              .querySelectorAll(`.${filterType}Count .filter`)
+              .forEach((link) => {
+                link.innerText = "🔎";
+                link.removeAttribute("filtered");
+              });
+            if (targetIsFiltered) {
+              displayedElements.forEach(
+                function (displayedElement) {
+                  this.removeHideCondition(displayedElement);
+                }.bind(this)
+              );
+              return;
             }
-          }.bind(this));
-        }.bind(this);
-        clicker.innerText = "🔎";
-        clicker.className = `filter`;
-        filterValueDiv.appendChild(clicker);
-      }
 
-      filterValueDiv.className = `${filterType}-issue-cnt-${issueInfo.count}`;
-      containerDiv.appendChild(filterValueDiv);
-    }.bind(this));
+            target.innerText = "✅";
+            target.setAttribute("filtered", true);
+            displayedElements.forEach(
+              function (displayedElement) {
+                if (displayedElement.innerText.includes(filterValue)) {
+                  this.removeHideCondition(displayedElement);
+                } else {
+                  this.addHideCondition(displayedElement);
+                }
+              }.bind(this)
+            );
+          }.bind(this);
+          clicker.innerText = "🔎";
+          clicker.className = `filter`;
+          filterValueDiv.appendChild(clicker);
+        }
+
+        filterValueDiv.className = `${filterType}-issue-cnt-${issueInfo.count}`;
+        containerDiv.appendChild(filterValueDiv);
+      }.bind(this)
+    );
     return containerDiv;
-
   }
 }
 export { toolbarRowFilterer };
