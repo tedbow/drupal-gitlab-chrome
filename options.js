0befe77cfbbd9fd3c06fec5931ddad9202cc13be
Ted Bowman <ted+git@tedbow.com>
2022-12-21 10:50:03 -0500

    add options

--- options.js
+++ options.js
@@ -1,23 +1,32 @@
 // Copied from https://developer.chrome.com/docs/extensions/mv3/options/
 // Saves options to chrome.storage
 
+function convertTextAreaToArray(element) {
+  return element
+      .value
+      .split(/\n/)
+      .map((line) => line.trim())
+      .filter((n) => n);
+}
+function setTextAreaValueByArray(element, items) {
+  element.value = items.join("\n");
+  element.setAttribute('rows', items.length + 1);
+}
 function save_options() {
-  const projects = document
-    .getElementById("projects")
-    .value.split(/\n/)
-    .map((line) => line.trim())
-    .filter((n) => n);
-  const auto_tags = document
-    .getElementById("auto_tags")
-    .value.split(/\n/)
-    .map((line) => line.trim())
-    .filter((n) => n);
+  const projects = convertTextAreaToArray(document.getElementById("projects"));
+  const auto_tags = convertTextAreaToArray(document.getElementById("auto_tags"));
   const load_pages = document.getElementById("load_pages").checked;
+  const project_test_requirements = {};
+  document.querySelectorAll('.project-tests').forEach(textArea => {
+    const project = textArea.getAttribute('project');
+    project_test_requirements[project] = convertTextAreaToArray(textArea);
+  });
 
   chrome.storage.sync.set(
     {
       projects: projects,
       load_pages: load_pages,
+      project_test_requirements: project_test_requirements,
       auto_tags: auto_tags,
     },
     function () {
@@ -37,6 +46,7 @@ function restore_options() {
   chrome.storage.sync.get(
     {
       projects: [],
+      project_test_requirements: {},
       load_pages: false,
       auto_tags: [],
     },
@@ -48,9 +58,26 @@ function restore_options() {
           "Accessibility",
         ];
       }
-      document.getElementById("projects").value = items.projects.join("\n");
-      document.getElementById("auto_tags").value = items.auto_tags.join("\n");
+      setTextAreaValueByArray(document.getElementById("projects"), items.projects);
+      setTextAreaValueByArray(document.getElementById("auto_tags"),items.auto_tags);
       document.getElementById("load_pages").checked = items.load_pages;
+      document.querySelectorAll('#project-container .project-tests-container').forEach(div => div.remove());
+      items.projects.forEach(project => {
+        const projectTestDiv = document.createElement('div');
+        projectTestDiv.classList.add('project-tests-container');
+        document.getElementById('project-container').append(projectTestDiv)
+        const projectTestLabel = document.createElement('label');
+        const projectTests = document.createElement('textarea');
+        if (items.project_test_requirements.hasOwnProperty(project)) {
+          setTextAreaValueByArray(projectTests, items.project_test_requirements[project])
+        }
+        projectTests.classList.add('project-tests');
+        projectTests.id = `project-tests-${project}`;
+        projectTests.setAttribute('project', project);
+        projectTestLabel.setAttribute('for', projectTests.id);
+        projectTestLabel.innerText = `Test requirements for '${project}`;
+        projectTestDiv.append(projectTestLabel, projectTests);
+      });
     }
   );
 }
